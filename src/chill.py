fe5b494ff16277afec59a8f0652a0b00c87d99a2
dikiardian <ardiandiki369@gmail.com>
2019-03-01 02:03:12 +0700

    init, padding plain text, padding key

---
+++ src/chill.py
@@ -0,0 +1,78 @@
+import numpy as np
+import random
+
+BLOCK_SIZE = 16 # bytes
+
+class Chill:
+  def __init__(self, plain_text_src = 'text', plain_text = '', plain_text_path = '', key = 'key'):
+    # constructor
+    self.key = key
+    self.plain_text = plain_text
+    self.plain_text_path = plain_text_path
+    self.plain_text_src = plain_text_src # ['text', 'file']
+    self.cipher_text = ''
+
+  def __to_hex(self, content):
+    result = ''
+    for c in content:
+      result += format(ord(c), '08b')
+    return '%08X' % int(result, 2)
+
+  def __plain_padding(self):
+    plain_block_size = 2*2*BLOCK_SIZE
+    if len(self.plain_text) % plain_block_size != 0:
+      padd_length = (plain_block_size - (len(self.plain_text) % plain_block_size)) / 2
+
+    padd_char = '%02X' % padd_length
+
+    for i in range(0, padd_length):
+      self.plain_text += padd_char
+
+  def __key_padding(self):
+    key_length = len(self.key)
+    if key_length == 2*BLOCK_SIZE:
+      pass
+    elif key_length < 2*BLOCK_SIZE:
+      seed = 0
+      for idx, k in enumerate(self.key):
+        if (idx % 2 == 1):
+          seed += ord(k)
+      random.seed(seed)
+      while len(self.key) < 2*BLOCK_SIZE:
+        pos = random.randrange(0, key_length)
+        self.key += self.key[pos]
+    else:
+      seed = 0
+      for idx, k in enumerate(self.key):
+        if (idx % 2 == 0):
+          seed += ord(k)
+      random.seed(seed)
+      while len(self.key) > 2*BLOCK_SIZE:
+        pos = random.randrange(0, key_length)
+        self.key = self.key[:pos] + self.key[(pos+1):]
+
+  def __load_plain_text(self):
+    # open file
+    with open(self.plain_text_path, mode='rb') as file:
+      file_content = file.read()
+
+    return file_content
+
+  def encrypt(self):
+    # get plain text
+    if self.plain_text_src == 'file':
+      self.plain_text = self.__load_plain_text()
+    
+    # convert plain text and key to hex
+    self.plain_text = self.__to_hex(self.plain_text)
+    self.key = self.__to_hex(self.key)
+
+    # padding plain text
+    self.__plain_padding()
+    # padding key
+    self.__key_padding()
+
+  def decrypt(self):
+    pass
+
+  
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from chill import Chill
+import random
+
+ch = Chill(plain_text_src='file', plain_text_path='../plain.txt', key='kunci gembok')
+
+ch.encrypt()
